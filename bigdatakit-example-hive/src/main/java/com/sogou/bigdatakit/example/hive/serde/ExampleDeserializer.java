b869f894e8b35e1e13379972bb160cf267b8045f
Tao Li <litao@sogou-inc.com>
2016-01-18 10:43:46 +0800

    Update ExampleDeserializer.

--- bigdatakit-example-hive/src/main/java/com/sogou/bigdatakit/example/hive/serde/ExampleDeserializer.java
+++ bigdatakit-example-hive/src/main/java/com/sogou/bigdatakit/example/hive/serde/ExampleDeserializer.java
@@ -14,20 +14,38 @@ public class ExampleDeserializer extends TextDeserializer {
   @Override
   public List<Object> deserialize(String line, List<Object> reuse) {
     String[] array = line.split(" ");
-    List<String> f4 = new ArrayList<String>();
-    for (String e : array[1].split(",")) {
-      f4.add(e);
+
+    // f0: string
+    String f0 = array[0];
+
+    // f1: int
+    int f1 = Integer.parseInt(array[1]);
+
+    // f2: array<string>
+    List<String> f2 = new ArrayList<>();
+    for (String e : array[2].split(",")) {
+      f2.add(e);
     }
-    Map<String, String> f3 = new HashMap<String, String>();
+
+    // f3: map<string,string>
+    Map<String, String> f3 = new HashMap<>();
     for (String kvStr : array[3].split("&")) {
       String[] kv = kvStr.split("=");
       f3.put(kv[0], kv[1]);
     }
 
-    reuse.add(0, Integer.parseInt(array[0]));
-    reuse.add(1, f4);
-    reuse.add(2, array[2]);
+    // f4: array<struct<pos:int,url:string>>
+    List<Object> f4 = new ArrayList<>();
+    for (String str : array[4].split(",")) {
+      String[] kv = str.split(":");
+      f4.add(new Object[]{Integer.parseInt(kv[0]), kv[1]});
+    }
+
+    reuse.add(0, f0);
+    reuse.add(1, f1);
+    reuse.add(2, f2);
     reuse.add(3, f3);
+    reuse.add(3, f4);
 
     return reuse;
   }
